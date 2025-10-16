5342e47fc22b175eee126f6d3676f3d68fb7b35b
dk-tencent <3162795401@qq.com>
2025-10-16 19:34:59 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,117 @@
+terraform {
+  required_providers {
+    ctcloud = {
+      source = "www.ctyun.cn/ctyun/ctcloud"
+      version = ">=1.0.0"
+    }
+  }
+}
+
+provider "ctcloud" {
+  ak                = var.ak
+  sk                = var.sk
+  env               = var.env
+  region_id         = var.region_id
+  master_order_id   = var.master_order_id
+  paas_resource_id  = var.paas_resource_id
+  account_id        = var.account_id
+  user_id           = var.user_id
+}
+
+locals {
+  security_rule_map = var.security_rules == null ? {} : { for idx, rule in tolist(var.security_rules) : "item-${idx}" => rule }
+  business_security_rule_map = var.business_security_rules == null ? {} : { for idx, rule in tolist(var.business_security_rules) : "item-${idx}" => rule }
+  computed_network_card_list = var.network_card_list == null ? null : toset([
+    for card in var.network_card_list : merge(card, {
+      security_group_id = var.enable_create_sec ? (card.paas_net_card_type == "manage" ? ctcloud_ecx_v2_security_group.security_group[0].security_group_id : ctcloud_ecx_v2_security_group.business_security_group[0].security_group_id) : card.security_group_id
+    })
+  ])
+
+
+}
+
+# 创建安全组
+resource "ctcloud_ecx_v2_security_group" "security_group" {
+  count       = var.enable_create_sec ? 1 : 0
+  account_id  = var.account_id
+  region_id   = var.region_id
+  name        = var.security_group_name
+  description = var.security_group_desc
+}
+
+# 创建安全组
+resource "ctcloud_ecx_v2_security_group" "business_security_group" {
+  count       = var.enable_create_sec ? 1 : 0
+  account_id  = var.business_account_id
+  is_ops      = var.is_ops
+  region_id   = var.region_id
+  name        = var.business_security_group_name
+  description = var.business_security_group_desc
+}
+
+# 创建安全组规则
+resource "ctcloud_ecx_v2_security_group_rule" "security_group_rules" {
+  depends_on        = [ctcloud_ecx_v2_security_group.security_group]
+  for_each          = local.security_rule_map
+  account_id        = var.account_id
+  region_id         = var.region_id
+  security_group_id = ctcloud_ecx_v2_security_group.security_group[0].security_group_id
+  direction         = each.value.direction
+  description       = each.value.description
+  ether_type        = each.value.ether_type
+  protocol          = each.value.protocol
+  multi_port        = each.value.range
+  dest_cidr_ip      = each.value.dest_cidr_ip
+  action            = each.value.action
+  priority          = each.value.priority
+}
+
+resource "ctcloud_ecx_v2_security_group_rule" "business_security_group_rules" {
+  depends_on        = [ctcloud_ecx_v2_security_group.business_security_group]
+  for_each          = local.business_security_rule_map
+  account_id        = var.business_account_id
+  is_ops            = var.is_ops
+  region_id         = var.region_id
+  security_group_id = ctcloud_ecx_v2_security_group.business_security_group[0].security_group_id
+  direction         = each.value.direction
+  description       = each.value.description
+  ether_type        = each.value.ether_type
+  protocol          = each.value.protocol
+  multi_port        = each.value.range
+  dest_cidr_ip      = each.value.dest_cidr_ip
+  action            = each.value.action
+  priority          = each.value.priority
+}
+
+# 创建ecs
+resource "ctcloud_ecx_v2_evm" "ecs_fw" {
+  count               = var.ecs_create ? 1:0
+  account_id          = var.account_id
+  region_id           = var.region_id
+  image_id            = var.image_id
+  flavor_id           = var.flavor_id
+  display_name        = var.display_name
+  user_password       = var.user_password
+  network_card_list   = local.computed_network_card_list
+  boot_disk_volume_type = var.boot_disk_volume_type
+  boot_disk_type      = var.boot_disk_type
+  boot_disk_size      = var.boot_disk_size
+  login_type          = var.login_type
+  specialized_type    = var.specialized_type
+  vpc_id              = var.vpc_id
+  charge_type         = var.charge_type
+  stop_type           = var.stop_type
+}
+
+resource "ctcloud_ecx_v2_evm_state" "ecx_v2_evm_state" {
+  count = var.enable_ecs_operation ? 1 : 0
+  action = var.action
+  instance_id = var.instance_id
+}
+
+data "ctcloud_ecx_evm_vnc" "ecx_evm_vnc" {
+  count = var.enable_ecs_vnc ? 1 : 0
+  account_id = var.account_id
+  instance_id = var.instance_id
+  region_id = var.region_id
+}
---
+++ variables.tf
@@ -0,0 +1,287 @@
+# 通用部分参数
+variable "ak" {
+  description = "ak"
+  type        = string
+  nullable    = false
+  default     = ""
+}
+variable "sk" {
+  description = "sk"
+  type        = string
+  nullable    = false
+  default     = ""
+}
+variable "env" {
+  description = "env"
+  type        = string
+  nullable    = false
+  default     = "ecxProd"
+}
+variable "master_order_id" {
+  description = "主订单id"
+  type        = string
+  nullable    = false
+  default     = ""
+}
+variable "paas_resource_id" {
+  type          = string
+  description   = "paas_resource_id"
+  nullable      = false
+  default       = ""
+}
+variable "account_id" {
+  type          = string
+  description   = "account_id"
+  nullable      = false
+  default       = ""
+}
+
+variable "user_id" {
+  type          = string
+  description   = "user_id"
+  nullable      = false
+  default       = ""
+}
+
+variable "business_account_id" {
+  type          = string
+  description   = "user_account_id"
+  nullable      = false
+  default       = ""
+}
+
+variable "region_id" {
+  description = "资源池id"
+  type        = string
+  nullable    = false
+  default     = ""
+}
+variable "vpc_id" {
+  description = "vpc id"
+  type        = string
+  nullable    = false
+  default     = ""
+}
+variable "subnet_id" {
+  description = "subnet id"
+  type        = string
+  nullable    = false
+  default     = ""
+}
+variable "enable_ipv6" {
+  description = "是否需要支持ipv6"
+  type        = bool
+  nullable    = false
+  default     = false
+}
+
+variable "is_ops" {
+  description = "是否是ops操作"
+  type        = bool
+  nullable    = true
+  default     = true
+}
+
+## 安全组变量
+variable "enable_create_sec" {
+  description = "是否创建安全组"
+  type        = bool
+  nullable    = false
+  default     = true
+}
+
+variable "security_group_id" {
+  description = "安全组id"
+  type        = string
+  nullable    = true
+  default     = null
+}
+
+variable "security_group_name" {
+  description = "管理网卡安全组名称"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "security_group_desc" {
+  description = "管理网卡安全组描述"
+  type        = string
+  default     = "防火墙管理安全组"
+  nullable    = true
+}
+variable "business_security_group_name" {
+  description = "业务网卡安全组名称"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "business_security_group_desc" {
+  description = "业务网卡安全组描述"
+  type        = string
+  default     = "防火墙业务安全组"
+  nullable    = true
+}
+## 安全组规则
+variable "security_rules" {
+  description     = "安全组规则"
+  type            = set(object({
+    direction     = string
+    action        = string
+    priority      = optional(number, 1)
+    protocol      = optional(string, "TCP")
+    ether_type    = optional(string, "IPv4")
+    dest_cidr_ip  = string
+    description   = optional(string, "管理网卡安全组规则")
+    range         = optional(string, "10443")
+  }))
+  default     = null
+  nullable    = true
+}
+
+variable "business_security_rules" {
+  description     = "业务网卡安全组规则"
+  type            = set(object({
+    direction     = string
+    action        = string
+    priority      = optional(number, 1)
+    protocol      = optional(string, "TCP")
+    ether_type    = optional(string, "IPv4")
+    dest_cidr_ip  = string
+    description   = optional(string, "业务网卡安全组规则")
+    range         = optional(string, "ALL")
+  }))
+  default     = null
+  nullable    = true
+}
+
+## 防火墙引擎机器
+variable "ecs_create" {
+  description = "是否创建"
+  type        = bool
+  nullable    = false
+  default     = true
+}
+
+
+variable "image_id" {
+  description = "镜像ID"
+  type        = string
+  nullable    = false
+}
+variable "flavor_id" {
+  description = "规格ID"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "display_name" {
+  description = "主机名称"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "user_password" {
+  description = "登陆密码"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "boot_disk_volume_type" {
+  description = "系统盘类型"
+  type        = string
+  nullable    = true
+  default     = "cloud"
+}
+variable "boot_disk_size" {
+  description = "系统盘大小"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "boot_disk_type" {
+  description = "系统盘IO类型"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "login_type" {
+  description = "登录方式"
+  type        = string
+  nullable    = true
+  default     = "password"
+}
+variable "specialized_type" {
+  description = "专用类型"
+  type        = string
+  nullable    = true
+  default     = "paas"
+}
+variable "charge_type" {
+  description = "虚机实例付费方式"
+  type        = string
+  nullable    = true
+  default     = "payForUse"
+}
+variable "evm_group" {
+  description = "需要加入的虚机组 ID"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "custom_data" {
+  description = "虚机自定义数据"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "stop_type" {
+  description = "关机类型"
+  type        = string
+  nullable    = true
+  default     = "SOFT"
+}
+
+
+variable "network_card_list" {
+  description               = "网卡相关配置"
+  type                      = set(object({
+    subnet_id               = string
+    security_group_id       = optional(string, "")
+    paas_net_card_type      = optional(string, "manage")
+    ctyun_acct_id           = string
+    eip_type                = optional(string)
+    ip_address              = optional(string)
+  }))
+  default     = null
+  nullable    = true
+}
+
+
+# ecs operation
+variable "action" {
+  description = "操作类型"
+  type        = string
+  nullable    = true
+  default     = ""
+}
+variable "instance_id" {
+  description = "虚机ID"
+  type        = string
+  nullable    = true
+  default     = ""
+}
+variable "enable_ecs_operation" {
+  description = "是否执行ecs操作，重启、关机等"
+  type        = bool
+  nullable    = true
+  default     = false
+}
+
+# ecs vnc
+variable "enable_ecs_vnc" {
+  description = "是否执行ecs vnc 查询"
+  type        = bool
+  nullable    = true
+  default     = false
+}
+
